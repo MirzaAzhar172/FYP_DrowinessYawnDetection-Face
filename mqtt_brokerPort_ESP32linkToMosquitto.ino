7957b6b25caf082bb40c514c9cc36a7611a8ede3
MirzaAzhar172 <130655399+MirzaAzhar172@users.noreply.github.com>
2024-05-05 09:34:22 +0800

    ESP32_ARDUINO IDE_Link to_MQTT Mosquitto Broker

---
+++ mqtt_brokerPort_ESP32linkToMosquitto.ino
@@ -0,0 +1,76 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "mirza2187!";
+const char* password = "buatbunyiudangdulu";
+const char* mqtt_server = "127.0.0.1";
+const char* alarm_topic = "Microsleep.org";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+    delay(10);
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+
+    for (int i = 0; i < length; i++) {
+        Serial.print((char)payload[i]);
+    }
+    Serial.println();
+
+    // You can add code here to trigger actions based on the received message
+}
+
+void reconnect() {
+    // Loop until we're reconnected
+    while (!client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        // Attempt to connect
+        if (client.connect("ESP32Client")) {
+            Serial.println("connected");
+            // Once connected, subscribe to the topic
+            client.subscribe(alarm_topic);
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            // Wait 5 seconds before retrying
+            delay(5000);
+        }
+    }
+}
+
+void setup() {
+    Serial.begin(9600);
+    setup_wifi();
+    client.setServer(mqtt_server, 1883);
+    client.setCallback(callback);
+}
+
+void loop() {
+    if (!client.connected()) {
+        reconnect();
+    }
+    client.loop();
+    // Add your other code here...
+}
